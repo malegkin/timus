b37463fe4c05459fe1581d4bedf722b057d14c1f
Malegkin <malegkin@gmail.com>
2018-07-02 22:43:31 +0300

    opt 1001: 0.093s 2036KB

--- src/1001/main.cpp
+++ src/1001/main.cpp
@@ -4,6 +4,23 @@
 #include <math.h>
 #include <string.h>
 
+float Q_rsqrt( double number )
+{
+	long i;
+	float x2, y;
+	const double threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+	return y;
+}
+
 
 int main()
 {    
@@ -11,18 +28,37 @@ int main()
     char buf[32];
     size_t out_size = 1024*1024;
     
-    uint64_t in;
-    for(int i = 0; i < 256*1000 && std::cin >> in; i++)
-    {
-        size_t n = sprintf( buf, "%.4f\n", sqrt( static_cast<double>( in )));
-        buf[n] = '\n';
-        out_size -= n;
-        memcpy(out + out_size, buf, n);
+    std::string line;
+    size_t in_size = 0;
+    while ( std::getline (std::cin, line) && in_size < 256*1024 ){
+        in_size += line.size();
+       
+        uint64_t in = .0;
+        bool is_number = false;
+        for(size_t i = 0; i <= line.size(); i++){
+            switch(line[i]) {
+                case '0' ... '9':
+                    in = (in * 10) + static_cast<uint64_t>(line[i] - '0');
+                    is_number = true;
+                break;
+                
+                default:
+                    if (is_number){
+                        size_t n = sprintf( buf, "%.4f\n", sqrt( in ));
+                        buf[n] = '\n';
+                        out_size -= n;
+                        memcpy(out + out_size, buf, n);
+                        is_number = false;
+                        in = 0;
+                    }
+            }
+        }
     }
-    
+               
     out[ 1024*1024 ] = '\0';
     
     std::cout << out + out_size;
+    free(out);
     
     return 0;
 }
