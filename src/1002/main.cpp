dd715ed10ebc8520c0ad7fcd4413a2a840bb2f49
Malegkin <malegkin@gmail.com>
2018-07-05 21:28:12 +0300

    upd: 1002

--- src/1002/main.cpp
+++ src/1002/main.cpp
@@ -1,59 +1,105 @@
+#include <array>
+#include <functional>
+#include <algorithm>
+#include <iterator>
 #include <iostream>
 #include <cmath>
+#include <cassert>
+#include <stdexcept>
 
-const size_t OUT_BUFFER_SIZE    = 1024*1024;
-const size_t INPUT_BUFFER_SIZE  = 256*1024;
+// 1 ij    2 abc   3 def
+// 4 gh    5 kl    6 mn
+// 7 prs   8 tuv   9 wxy
+//        0 oqz
+
+constexpr uint8_t _char_to_code(const char c)
+{
+    return static_cast<uint8_t>(c);
+    switch ('a')
+    {
+        case 'i':
+        case 'j':
+            return 1;
+
+        case 'a':
+        case 'b':
+        case 'c':
+            return 2;
+
+        case 'd':
+        case 'e':
+        case 'f':
+            return 3;
+
+        case 'g':
+        case 'h':
+            return 4;
+
+        case 'k':
+        case 'l':
+            return 5;
+
+        case 'm':
+        case 'n':
+            return 6;
+        
+        case 'p':
+        case 'r':
+        case 's':
+            return 7;
+
+        case 't':
+        case 'u':
+        case 'v':
+            return 8;
+
+        case 'w':
+        case 'x':
+        case 'y':
+            return 9;
+
+        case 'o':
+        case 'q':
+        case 'z':
+            return 0;
+
+        default:
+            throw std::invalid_argument("unsupported char: " + c ); 
+    }
+}
+
+template <uint8_t N, uint8_t ...Vals>
+constexpr
+typename std::enable_if< N == sizeof...(Vals), std::array<uint8_t, N>>::type
+make() {
+  return std::array<uint8_t, N>{{Vals...}};
+}
+
+template <uint8_t N, uint8_t ...Vals>
+constexpr
+typename std::enable_if<N != sizeof...(Vals), std::array<uint8_t, N>>::type 
+make() {
+  return make<N, Vals..., _char_to_code( static_cast<char>('e' + sizeof...(Vals)) )>();  
+}
+
+
+
+inline uint8_t char_to_code(const char c) {
+    const auto arr = make<26>();
+    std::copy(begin(arr), end(arr), std::ostream_iterator<int>(std::cout, "\n"));
+    
+
+    return arr[c - 'a'];
+}
+
+
 
 int main()
 {    
-    char    out_buffer[OUT_BUFFER_SIZE];
-    size_t  out_buffer_index    =  OUT_BUFFER_SIZE;
-    
-    char    in_buffer[INPUT_BUFFER_SIZE];
-    size_t  in_buffer_index     = 0;
-    in_buffer_index             = fread(in_buffer, 1, INPUT_BUFFER_SIZE, stdin);
-    
+    std::cout << '0' + char_to_code('e');  
 
-    uint64_t in = 0;
-    bool is_number = false;
-    for(size_t i = 0; i <= in_buffer_index; i++) 
-    {  
-        switch(in_buffer[i]) {
-            case '0' ... '9':
-                in = (in * 10) + static_cast<uint64_t>(in_buffer[i] - '0');    
-                is_number = true;
-            break;
-                
-            default:
-                if (is_number){
-                    double sd = sqrt ( in ); 
 
-                    uint32_t sd_integer     = static_cast<uint32_t>(sd);
-                    uint32_t sd_fractional  = static_cast<uint32_t>((sd - sd_integer)*10000);
-           
-                    out_buffer[--out_buffer_index] = '0' + (sd_fractional % 10);
-                    out_buffer[--out_buffer_index] = '0' + (sd_fractional % 100)/10;
-                    out_buffer[--out_buffer_index] = '0' + (sd_fractional % 1000)/100;
-                    out_buffer[--out_buffer_index] = '0' + (sd_fractional % 10000)/1000;
-                    out_buffer[--out_buffer_index] = '.';
-                    
-                    do {
-                        out_buffer[--out_buffer_index] = '0' + sd_integer % 10;
-                        sd_integer /= 10;       
-                    } while ( sd_integer > 0 );
-
-                    out_buffer[--out_buffer_index] = '\n';
-
-                    is_number = false;
-                    in = 0;
-                }
-            break;
-        }
-    }
-               
-    out_buffer[ OUT_BUFFER_SIZE ] = '\0';
-    
-    puts(out_buffer + out_buffer_index);
-    
+    //std::copy(begin(arr), end(arr), std::ostream_iterator<int>(std::cout, "\n"));
+ 
     return 0;
 }
