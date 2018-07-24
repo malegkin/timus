27bbc54d285fe9abec13c4cada135e4c897500f3
Malegkin <malegkin@gmail.com>
2018-07-24 23:00:48 +0300

    add ytcf

---
+++ src/ytcf2/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+int main()
+{    
+    uint32_t n;
+    uint32_t m;
+    std::cin >> n >> m;
+    
+    std::vector<int64_t> v;
+    v.reserve(m+1);
+    for (int i =0; i <= m; i++){
+        v[i] = 0;
+    }
+
+    for (int i = 0; i < n-1; i++){
+        char id[10];
+        uint32_t d, c;
+        scanf("%10s%d%d", id, &d, &c);
+
+        switch (id[0]){
+            case 'a':
+                for (int j = d; j <= m; j++){
+                    v[j] += c;
+                }
+                break;
+            case 'd':
+                for (int j = d; j <= m; j++){
+                    v[j] -= c;
+                }
+                break;
+
+        }
+    }
+
+
+    uint64_t pm = 0;
+    uint64_t pk = 0;
+
+    for (int i=1; i <= m; i += 2){
+        //std::cout << "for: " << i << "p: " << v[i]  << std::endl;
+        pm = (v[i]>pm)?v[i]:pm;
+    }
+
+    for (int i=2; i <= m; i += 2){
+        //std::cout << "for: " << i << "p: " << v[i]  << std::endl;
+        pk = (v[i]>pk)?v[i]:pk;
+    }
+
+    printf("%ld %ld", pm, pk);
+
+    return 0;
+}
