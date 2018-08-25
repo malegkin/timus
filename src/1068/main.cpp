ec2def6bc4cc54c9b45048530129e3596f1c1d22
Malegkin <malegkin@gmail.com>
2018-08-25 15:04:19 +0300

    timus 1068: minor code style update

--- src/1068/main.cpp
+++ src/1068/main.cpp
@@ -6,7 +6,7 @@ int main()
     int32_t N;
     std::cin >> N;
 
-    std::cout << (1 + N)*(abs(N - 1)+1)/2 << std::endl;
+    std::cout << (1 + N)*(1 + abs(N - 1))/2 << std::endl;
 
     return 0;
 }
