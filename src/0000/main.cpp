cc5437cc9b6391e56e6dcbe81aa1dd236dde2fea
Malegkin <malegkin@gmail.com>
2018-08-27 09:37:18 +0300

    update timus solution template

--- src/0000/main.cpp
+++ src/0000/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <string>
+#include <cassert>
+
+std::string process(const std::string& in){
+    std::string out;
+
+    return out;
+}
 
 std::string process(const uint32_t N){
     std::string out;
@@ -9,9 +16,16 @@ std::string process(const uint32_t N){
 
 int main()
 {
+    #ifndef ONLINE_JUDGE
+        freopen("test.cin", "rt", stdin);
+
+        std::string test_in = "";
+        std::string test_out = "";
+        assert(process(test_in) == test_out);
+    #endif
+
     uint32_t N;
     std::cin >> N;
-
 
     std::cout << process(N) << std::endl;
 
