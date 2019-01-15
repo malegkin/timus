df98168efa18c578ed3080e0e97d5a243a2c7bbd
Malegkin <malegkin@gmail.com>
2019-01-15 23:39:06 +0300

    add: timus.1792.cpp

---
+++ src/1792/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <sstream>
+#include <iterator>
+#include <cassert>
+#include <string>
+#include <algorithm>
+#include <array>
+
+std::string solve(std::istream &in) {
+    std::array<bool, 7> ina;
+    for(size_t i = 0; i < 7; i++){
+        in >> ina[i];
+    }
+
+    bool r1 = ina[1] xor ina[2] xor ina[3];
+    bool r2 = ina[0] xor ina[2] xor ina[3];
+    bool r3 = ina[0] xor ina[1] xor ina[3];
+
+    if ( r1 != ina[4] && r2 != ina[5] && r3 == ina[6])  ina[0] = !ina[0];  
+    if ( r1 != ina[4] && r2 == ina[5] && r3 != ina[6])  ina[1] = !ina[1];  
+    if ( r1 == ina[4] && r2 != ina[5] && r3 != ina[6])  ina[2] = !ina[2];  
+    if ( r1 != ina[4] && r2 != ina[5] && r3 != ina[6])  ina[3] = !ina[3];  
+
+    if ( r1 != ina[4] && r2 == ina[5] && r3 == ina[6])  ina[4] = !ina[4];  
+    if ( r1 == ina[4] && r2 != ina[5] && r3 == ina[6])  ina[5] = !ina[5];  
+    if ( r1 == ina[4] && r2 == ina[5] && r3 != ina[6])  ina[6] = !ina[6];  
+    
+
+    std::stringstream out;
+    std::copy(std::begin(ina), std::end(ina), std::ostream_iterator<bool>(out, " "));
+    return out.str();
+
+}
+
+std::string solve(std::string in){
+    std::stringstream ss(in);
+    return solve(ss);
+}
+
+int main()
+{
+    assert(solve("1 1 1 0 0 0 1") == "1 1 1 0 0 0 0 ");
+    #ifndef ONLINE_JUDGE
+        freopen("./tests/2.in", "rt", stdin);
+    #endif
+
+    std::cout << solve( std::cin ) << std::endl;
+
+    return 0;
+}
---
+++ src/1792/run.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+for file in  $DIR/tests/*.in; do
+    echo -n "START TEST: [$(basename $file .in)] "
+
+    out=$(cat "$file" | "$DIR/a.out" | sed 's/[[:blank:]]*$//')
+
+    etalon=$(cat "$DIR/tests/$(basename "$file" .in).out")
+
+    if [ "$out" != "$etalon" ]; then
+        echo "........ [FAIL]"
+        echo "    OUT: [$out]"
+        echo "    ETN: [$etalon]"
+        exit -1
+    fi
+
+    echo ".........[DONE]"
+done
