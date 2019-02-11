9d073cba4ba1fbdd590cb6d0b60174e3053ef594
Malegkin <malegkin@gmail.com>
2019-02-11 21:33:35 +0300

    add: timus.2068.cpp

---
+++ src/2068/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+
+int main()
+{
+    size_t n;
+    std::cin >> n;
+    //cos both play optimally 
+    // (2*N + 1) => 1 + 1 + (2*(N-1)+1)
+    // each heaps can be optimally divides to mininal sub-heaps in N steps
+    size_t steps = 0;
+    while (n--) {
+        size_t h;
+        std::cin >> h;
+        steps += h / 2;    
+    }
+
+    // Daenerys has dragons so she moves First &&
+    // The player who cannot make a move loses => 
+    // if optimal steps count is odd number => Sannis win
+
+    if (steps % 2)
+        std::cout << "Daenerys" << std::endl;
+    else
+        std::cout << "Stannis" << std::endl;
+    
+    return 0;
+}
