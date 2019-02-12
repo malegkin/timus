21989501137252e78dc1f01863e500e0230bf9cc
Malegkin <malegkin@gmail.com>
2019-02-13 01:53:57 +0300

    up timus.1117.cpp corrected after rsdn review
    - using constexpr for constants
    - fix invalid timus number

--- src/1712/main.cpp
+++ src/1712/main.cpp
@@ -4,57 +4,57 @@
 #include <algorithm>
 #include <iterator>
 
-static const size_t SQUARE_DIMENSION = 4;
-using square_t = std::array<std::string, SQUARE_DIMENSION>;
+constexpr size_t TIMUS_1712_SQUARE_DIMENSION = 4;
+using square_t = std::array<std::string, TIMUS_1712_SQUARE_DIMENSION>;
 
 template <typename F>
-void repeat(unsigned n, F f) {
+void repeat(unsigned n, F&& f) {
     while (n--) f();
 }
 
 inline square_t read_square(std::istream &in){
     square_t out;
     
-    std::generate_n( std::begin(out), SQUARE_DIMENSION, [&](){
+    std::generate( std::begin(out), std::end(out), [&](){
         std::string line;
+        line.reserve( TIMUS_1712_SQUARE_DIMENSION );
         getline(in, line);
-        return std::move( line ); 
+        return line; 
     });
 
-    return std::move( out );
+    return out;
 }
 
-inline square_t& rotate(square_t& in){
+inline void rotate(square_t& in){
 
-    for (size_t i = 0; i < SQUARE_DIMENSION; i++) {
-        for (size_t j = i; j < SQUARE_DIMENSION; j++) {
+    for (size_t i = 0; i < TIMUS_1712_SQUARE_DIMENSION; i++) {
+        for (size_t j = i; j < TIMUS_1712_SQUARE_DIMENSION; j++) {
             std::swap( in[i][j], in[j][i] ); 
         }
     }
 
 
-    for(size_t i = 0; i < SQUARE_DIMENSION/2; i++) {
-        for(size_t j = 0; j < SQUARE_DIMENSION; j++) {
-            std::swap( in[j][i], in[j][SQUARE_DIMENSION - i - 1] );
+    for(size_t i = 0; i < TIMUS_1712_SQUARE_DIMENSION/2; i++) {
+        for(size_t j = 0; j < TIMUS_1712_SQUARE_DIMENSION; j++) {
+            std::swap( in[j][i], in[j][TIMUS_1712_SQUARE_DIMENSION - i - 1] );
         }
     }
-    
-    return in;
 }
 
 
 inline std::string merge(const square_t& ciphered, const square_t& grille){
     std::string out;
+    out.reserve( TIMUS_1712_SQUARE_DIMENSION*TIMUS_1712_SQUARE_DIMENSION );
 
-    for (size_t i = 0; i < SQUARE_DIMENSION; i++){
-        for (size_t j = 0; j < SQUARE_DIMENSION; j++){
+    for (size_t i = 0; i < TIMUS_1712_SQUARE_DIMENSION; i++){
+        for (size_t j = 0; j < TIMUS_1712_SQUARE_DIMENSION; j++){
             if ( grille[i][j] == 'X' )
                 out += ciphered[i][j];
         }
     }
     
 
-    return std::move( out );
+    return out;
 }
 
 
@@ -65,8 +65,8 @@ int main()
 
 
     repeat(4, [&](){
-        std::cout << merge( ciphered, grille);
-        rotate(grille);
+        std::cout << merge( ciphered, grille );
+        rotate( grille );
     });
 
     return 0;
