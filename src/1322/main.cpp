cc16f1c87ef40f5b06640889c4088c4c3007483f
Malegkin <malegkin@gmail.com>
2019-03-19 23:01:27 +0300

    add: timus.1322

---
+++ src/1322/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <cassert>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    size_t n;
+    std::string in;
+    std::cin >> n >> in;
+
+    std::cout << in << std::endl;
+
+
+    return 0;
+}
